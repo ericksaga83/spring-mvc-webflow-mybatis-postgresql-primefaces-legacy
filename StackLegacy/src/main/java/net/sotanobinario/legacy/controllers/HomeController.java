d68084ac2b68d416bc0a7df21ec55d84fb53cb19
Erick SaGa <esanchez@saganet.com.mx>
2019-03-05 14:15:04 -0600

    Configuración Base del Stack

---
+++ StackLegacy/src/main/java/net/sotanobinario/legacy/controllers/HomeController.java
@@ -0,0 +1,33 @@
+package net.sotanobinario.legacy.controllers;
+
+import java.text.DateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class HomeController {
+	
+	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
+	
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public String home(Locale locale, Model model) {
+		logger.debug("Bienvenid@! La hora local del cliente es {}.", locale);
+		
+		Date date = new Date();
+		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
+		
+		String formattedDate = dateFormat.format(date);
+		
+		model.addAttribute("horaServidor", formattedDate );
+		
+		return "home";
+	}
+	
+}
